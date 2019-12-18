65d263cb55a9cf7c919e01b814cfabb7b81f6015
Berkay <mailberkayoztunc@gmail.com>
2019-12-19 00:40:36 +0300

    policy alert added

--- src/app/views/policy/policyTransection/policy-transection.module.ts
+++ src/app/views/policy/policyTransection/policy-transection.module.ts
@@ -3,15 +3,14 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared.module';
 
 @NgModule({
     imports: [
         CommonModule,
         TranslateModule,
         NgbModule,
-        FormsModule,
-        ReactiveFormsModule
+        SharedModule
     ],
     declarations: []
 
